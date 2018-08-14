9d49c66bd243637843f191ebc96520e3bb552fde
jtberghuis <41877847+jtberghuis@users.noreply.github.com>
2018-08-14 10:24:45 -0500

    Add files via upload

---
+++ PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  InstagramUI
+//
+//  Created by Jordan Berghuis on 8/13/18.
+//  Copyright © 2018 Developers Academy. All rights reserved.
+//
+
+import UIKit
+class PostCell: UITableViewCell {
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var numberOfLikesButton: UIButton!
+    @IBOutlet weak var timeAgoLabel: UILabel!
+    @IBOutlet weak var postCaptionLabel: UILabel!
+    
+    var post: Post! {
+        didSet {
+            self.updateUI()
+        }
+    }
+    func updateUI()
+    {
+        self.postImageView.image = post.image
+        postCaptionLabel.text = post.caption
+        numberOfLikesButton.setTitle("Be the first one to share a comment", for: [])
+        timeAgoLabel.text = post.timeAgo
+    }
+}
